1eab737ad853a4f2c3d51870695c96baf71613a7
Nikhil1141 <114666557+Nikhil1141@users.noreply.github.com>
2023-10-06 15:19:32 +0530

    Update strings.js

--- js/strings.js
+++ js/strings.js
@@ -1,87 +1,45 @@
-// q1) Find the first occurrence of a character in a given string.
-// var a = "Nikhileswar";
-// var b = a.at(0);
-// console.log(b)
+q1) Find the first occurrence of a character in a given string.
+var a = "Nikhileswar";
+var b = a.at(0);
+console.log(b)
 
 
-// q2) Find the last occurrence of a character in a given string.
-// var a = "Nikhileswar";
-// var b = a.at(-1);
-// console.log(b)
-
-
-
-// q3) Search all occurrences of a character in a given string.
-// var a = "Nikhileswar";
-// var b = a.slice();
-// console.log(b)
-
-
-// q4) Count occurrences of a character in a given string.
-// var a = "Nikhileswar";
-// var count = 0;
-// for(var i = 0;i<=a.length-1;i++){
-//     var b = a.slice();
-//     count++;
-// }
-// console.log(b)
-// console.log(count);
-
-
-// q5) Find the highest frequency character in a string.
+q2) Find the last occurrence of a character in a given string.
+var a = "Nikhileswar";
+var b = a.at(-1);
+console.log(b)
 
 
 
-// q6) Find the lowest frequency character in a string.
+q3) Search all occurrences of a character in a given string.
+var a = "Nikhileswar";
+var b = a.slice();
+console.log(b)
 
 
+q4) Count occurrences of a character in a given string.
+var a = "Nikhileswar";
+var count = 0;
+for(var i = 0;i<=a.length-1;i++){
+    var b = a.slice();
+    count++;
+}
+console.log(b)
+console.log(count);
 
-// q7) Count the frequency of each character in a string.
+q8) Remove the first occurrence of a character from a string.
+var a = "Nikhileswar";
+var b = a.slice(1);
+console.log(b)
 
 
-
-// q8) Remove the first occurrence of a character from a string.
-// var a = "Nikhileswar";
-// var b = a.slice(1);
-// console.log(b)
-
-
-// q9) Remove the last occurrence of a character from a string.
+q9) Remove the last occurrence of a character from a string.
 var a = "Nikhileswar";
 var b = a.pop('N');
 console.log(b)
 
 
-// q10) Delete all occurrences of a character from a string.
-// var a = "Nikhileswar";
-// var b = a.split().slice();
-// console.log(b)
-
-
-// q11) Remove all repeated characters from a given string.
-
-
-
-// q12) Replace the first occurrence of a character with another in a string.
-
-
-
-// q13) Replace the last occurrence of a character with another in a string.
-
-
-
-// q14) Put all occurrences of a character with another in a string.
-
-
-
-// q15) Find the first occurrence of a word in a given string.
-
-
-
-// q16) Find the last occurrence of a word in a given string.
-
-
-
-// q17) Search all occurrences of a word in a given string.
-
-
+q10) Delete all occurrences of a character from a string.
+var a = "Nikhileswar";
+var b = a.split().slice();
+console.log(b)
